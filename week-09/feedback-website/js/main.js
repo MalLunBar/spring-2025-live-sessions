a12f579ed1866915f56082cdefe9f460899c4588
MalLunBar <malinelundgren1991@gmail.com>
2025-04-02 13:50:08 +0200

    fix

--- week-09/feedback-website/js/main.js
+++ week-09/feedback-website/js/main.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', () => {
   const introSection = document.getElementById('introduction');
   const userInfoSection = document.getElementById('user-info');
